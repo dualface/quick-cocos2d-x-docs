37225680bf96266e3b0f3e2584f407e284e3a8dc
dualface <dualface@gmail.com>
2014-05-28 09:50:50 +0800

    update gen script

--- generate_local_html.sh
+++ generate_local_html.sh
@@ -2,12 +2,28 @@
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 SOURCE_DIR="$QUICK_COCOS2DX_ROOT/framework"
 CONFIG_PATH="$QUICK_COCOS2DX_ROOT/docs/apidoc_config.json"
-TEMP_DIR="$DIR/tmp/"
-DOC_DIR="$QUICK_COCOS2DX_ROOT/docs/api/"
+TEMP_DIR="$DIR/tmp"
+DOC_DIR="$DIR/api"
+QUICK_DOC_DIR="$QUICK_COCOS2DX_ROOT/docs/api"
 
 if [ -d "$TEMP_DIR" ]; then
     rm -r "$TEMP_DIR"
 fi
 
+if [ -f "$DOC_DIR/*" ]; then
+    rm "$DOC_DIR/*"
+fi
+
+if [ -f "$QUICK_DOC_DIR/*" ]; then
+    rm "$QUICK_DOC_DIR/*"
+fi
+
 php "$LUADOCX" extract -c "$CONFIG_PATH" "$SOURCE_DIR" "$TEMP_DIR"
 php "$LUADOCX" generate -c "$CONFIG_PATH" "$TEMP_DIR" "$DOC_DIR"
+
+cd "$DIR"
+git add api
+git commit -m "update apidoc"
+
+cp "$DOC_DIR/"* "$QUICK_DOC_DIR"
+
